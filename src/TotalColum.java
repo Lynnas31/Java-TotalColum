ede0f3b3f6e3ca840fb1875d29428eb8db1346a9
Hien_Nguyen <h.phuong211296@gmail.com>
2021-02-18 13:42:50 +0700

    1

--- src/TotalColum.java
+++ src/TotalColum.java
@@ -1,6 +1,6 @@
 public class TotalColum {
     public static void main(String[] args) {
-        int[][] Array = {{787, 214, 0, 888, 1549, 1548, 1627}, {714, 1102, 888, 0, 661, 781, 810},{1,2,3,4,5,6,7}};
+        int[][] Array = {{743, 414, 2341, 42138, 412329, 43128, 6577}, {3214, 3202, 438, 320, 1231, 1, 0},{1,2,3,4,5,6,7}};
         for (int i = 0; i < Array[0].length; i++) {
             int total = 0;
             for (int j = 0; j < Array.length; j++) {
